4ee5be1edaab4d0c5dcf13becc1ee097a4fad77c
AshishLakhimale23 <ashishlakhimale23@gmail.com>
2025-02-07 11:41:13 +0530

    initialized a turbo repo

---
+++ apps/http-server/src/index.ts
@@ -0,0 +1,4 @@
+import express from "express";
+const app = express()
+app.use(express.json())
+app.listen(8000,()=>console.log("server started at 8000"))
---
+++ apps/ws-server/src/index.ts
@@ -0,0 +1,4 @@
+import {WebSocketServer } from "ws"
+const websocket = new WebSocketServer({port:8001})
+
+console.log("server started")
