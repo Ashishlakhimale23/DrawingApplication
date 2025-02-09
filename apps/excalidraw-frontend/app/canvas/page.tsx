7c645edef828d71d5f00fad6a7dd1d0c36aab341
AshishLakhimale23 <ashishlakhimale23@gmail.com>
2025-02-09 16:08:22 +0530

    structured a bit

--- apps/excalidraw-frontend/app/canvas/page.tsx
+++ apps/excalidraw-frontend/app/canvas/page.tsx
@@ -1,6 +1,8 @@
 "use client"
 import { MouseEvent, useEffect, useRef, useState } from "react"
 import { reDrawing,Draw } from "@/draw/Drawing"
+import { GetSelectedShape } from "@/draw/SelectedShape"
+import { Resize,getResizeEdge } from "@/draw/Resizing"
 
 
 interface Shape {
@@ -62,136 +64,7 @@ export default function Canvas(){
     });
   }, []);
 
-  const GetSelectedShape = (shape: Shape, mouseX: number, mouseY: number) => {
-    const tolerance = 10;
-
-    const minX = Math.min(shape.x, shape.x + shape.width);
-    const maxX = Math.max(shape.x, shape.x + shape.width);
-    const minY = Math.min(shape.y, shape.y + shape.height);
-    const maxY = Math.max(shape.y, shape.y + shape.height);
-    return (
-      (Math.abs(mouseX - minX) <= tolerance &&
-        minY <= mouseY &&
-        mouseY <= maxY) ||
-      (Math.abs(mouseY - minY) <= tolerance &&
-        minX <= mouseX &&
-        mouseX <= maxX) ||
-      (Math.abs(mouseX - maxX) <= tolerance &&
-        minY <= mouseY &&
-        mouseY <= maxY) ||
-      (Math.abs(mouseY - maxY) <= tolerance && minX <= mouseX && mouseX <= maxX)
-    );
-  };
-
-  function getResizeEdge(mouseX: number, mouseY: number, shape: Shape) {
-    const { x, y, selected, width, height } = shape;
-    const threshold = 10;
-
-    if (!selected) {
-      return null;
-    }
-
-    if (
-      Math.abs(mouseX - (x + width)) < threshold &&
-      Math.abs(mouseY - (y + height)) < threshold
-    ) {
-      return "bottom-right";
-    }
-    if (Math.abs(mouseX - x) < threshold && Math.abs(mouseY - y) < threshold) {
-      return "top-left";
-    }
-    if (
-      Math.abs(mouseX - (x + width)) < threshold &&
-      Math.abs(mouseY - y) < threshold
-    ) {
-      return "top-right";
-    }
-    if (
-      Math.abs(mouseX - x) < threshold &&
-      Math.abs(mouseY - (y + height)) < threshold
-    ) {
-      return "bottom-left";
-    }
-
-    if (Math.abs(mouseX - x) < threshold) {
-      return "left";
-    }
-    if (Math.abs(mouseX - (x + width)) < threshold) {
-      return "right";
-    }
-    if (Math.abs(mouseY - y) < threshold) {
-      return "top";
-    }
-    if (Math.abs(mouseY - (y + height)) < threshold) {
-      return "bottom";
-    }
-
-    return null;
-  }
-
-  const Resize = (mouseX: number, mouseY: number) => {
-    if (
-      SelectedIndex.current === -1 ||
-      !shapes.current[SelectedIndex.current]
-    ) {
-      return;
-    }
-
-    ctxRef.current?.clearRect(0,0,canvasRef.current?.width,canvasRef.current?.height)
-    switch (shapes.current[SelectedIndex.current].resizingEdge) {
-      case "top-left":
-        shapes.current[SelectedIndex.current].width +=
-          shapes.current[SelectedIndex.current].x - mouseX;
-        shapes.current[SelectedIndex.current].height +=
-          shapes.current[SelectedIndex.current].y - mouseY;
-        shapes.current[SelectedIndex.current].x = mouseX;
-        shapes.current[SelectedIndex.current].y = mouseY;
-        break;
-      case "top-right":
-        shapes.current[SelectedIndex.current].width =
-          mouseX - shapes.current[SelectedIndex.current].x;
-        shapes.current[SelectedIndex.current].height +=
-          shapes.current[SelectedIndex.current].y - mouseY;
-        shapes.current[SelectedIndex.current].y = mouseY;
-        break;
-      case "bottom-left":
-        shapes.current[SelectedIndex.current].width +=
-          shapes.current[SelectedIndex.current].x - mouseX;
-        shapes.current[SelectedIndex.current].height =
-          mouseY - shapes.current[SelectedIndex.current].y;
-        shapes.current[SelectedIndex.current].x = mouseX;
-        break;
-      case "bottom-right":
-        shapes.current[SelectedIndex.current].width =
-          mouseX - shapes.current[SelectedIndex.current].x;
-        shapes.current[SelectedIndex.current].height =
-          mouseY - shapes.current[SelectedIndex.current].y;
-        break;
-      case "left":
-        shapes.current[SelectedIndex.current].width +=
-          shapes.current[SelectedIndex.current].x - mouseX;
-        shapes.current[SelectedIndex.current].x = mouseX;
-        break;
-      case "right":
-        shapes.current[SelectedIndex.current].width =
-          mouseX - shapes.current[SelectedIndex.current].x;
-        break;
-      case "top":
-        shapes.current[SelectedIndex.current].height +=
-          shapes.current[SelectedIndex.current].y - mouseY;
-        shapes.current[SelectedIndex.current].y = mouseY;
-        break;
-      case "bottom":
-        shapes.current[SelectedIndex.current].height =
-          mouseY - shapes.current[SelectedIndex.current].y;
-        break;
-    }
-
-    InitialPoints.current.x = mouseX;
-    InitialPoints.current.y = mouseY
-
-    reDrawing(ctxRef, canvasRef, shapes);
-  };
+  
 
 
   const MouseDown = (e: MouseEvent) => {
@@ -249,6 +122,7 @@ export default function Canvas(){
       Draw(ctxRef, canvasRef, InitialPoints, MovingPoints,typeOfShapes);
     } else {
       Resize(e.clientX, e.clientY);
+      reDrawing(ctxRef,canvasRef,shapes)
     }
   };
 
---
+++ apps/excalidraw-frontend/draw/Resizing.ts
@@ -0,0 +1,106 @@
+export function getResizeEdge(mouseX: number, mouseY: number, shape: Shape) {
+    const { x, y, selected, width, height } = shape;
+    const threshold = 10;
+
+    if (!selected) {
+      return null;
+    }
+
+    if (
+      Math.abs(mouseX - (x + width)) < threshold &&
+      Math.abs(mouseY - (y + height)) < threshold
+    ) {
+      return "bottom-right";
+    }
+    if (Math.abs(mouseX - x) < threshold && Math.abs(mouseY - y) < threshold) {
+      return "top-left";
+    }
+    if (
+      Math.abs(mouseX - (x + width)) < threshold &&
+      Math.abs(mouseY - y) < threshold
+    ) {
+      return "top-right";
+    }
+    if (
+      Math.abs(mouseX - x) < threshold &&
+      Math.abs(mouseY - (y + height)) < threshold
+    ) {
+      return "bottom-left";
+    }
+
+    if (Math.abs(mouseX - x) < threshold) {
+      return "left";
+    }
+    if (Math.abs(mouseX - (x + width)) < threshold) {
+      return "right";
+    }
+    if (Math.abs(mouseY - y) < threshold) {
+      return "top";
+    }
+    if (Math.abs(mouseY - (y + height)) < threshold) {
+      return "bottom";
+    }
+
+    return null;
+  }
+
+export  const Resize = (mouseX: number, mouseY: number) => {
+    if (
+      SelectedIndex.current === -1 ||
+      !shapes.current[SelectedIndex.current]
+    ) {
+      return;
+    }
+
+    ctxRef.current?.clearRect(0,0,canvasRef.current?.width,canvasRef.current?.height)
+    switch (shapes.current[SelectedIndex.current].resizingEdge) {
+      case "top-left":
+        shapes.current[SelectedIndex.current].width +=
+          shapes.current[SelectedIndex.current].x - mouseX;
+        shapes.current[SelectedIndex.current].height +=
+          shapes.current[SelectedIndex.current].y - mouseY;
+        shapes.current[SelectedIndex.current].x = mouseX;
+        shapes.current[SelectedIndex.current].y = mouseY;
+        break;
+      case "top-right":
+        shapes.current[SelectedIndex.current].width =
+          mouseX - shapes.current[SelectedIndex.current].x;
+        shapes.current[SelectedIndex.current].height +=
+          shapes.current[SelectedIndex.current].y - mouseY;
+        shapes.current[SelectedIndex.current].y = mouseY;
+        break;
+      case "bottom-left":
+        shapes.current[SelectedIndex.current].width +=
+          shapes.current[SelectedIndex.current].x - mouseX;
+        shapes.current[SelectedIndex.current].height =
+          mouseY - shapes.current[SelectedIndex.current].y;
+        shapes.current[SelectedIndex.current].x = mouseX;
+        break;
+      case "bottom-right":
+        shapes.current[SelectedIndex.current].width =
+          mouseX - shapes.current[SelectedIndex.current].x;
+        shapes.current[SelectedIndex.current].height =
+          mouseY - shapes.current[SelectedIndex.current].y;
+        break;
+      case "left":
+        shapes.current[SelectedIndex.current].width +=
+          shapes.current[SelectedIndex.current].x - mouseX;
+        shapes.current[SelectedIndex.current].x = mouseX;
+        break;
+      case "right":
+        shapes.current[SelectedIndex.current].width =
+          mouseX - shapes.current[SelectedIndex.current].x;
+        break;
+      case "top":
+        shapes.current[SelectedIndex.current].height +=
+          shapes.current[SelectedIndex.current].y - mouseY;
+        shapes.current[SelectedIndex.current].y = mouseY;
+        break;
+      case "bottom":
+        shapes.current[SelectedIndex.current].height =
+          mouseY - shapes.current[SelectedIndex.current].y;
+        break;
+    }
+    
+  };
+
